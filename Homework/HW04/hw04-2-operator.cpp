27ae78ab2d4b0b184c1c1194ea9a77fec0594e10
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-08-10 13:16:42 +0700

    Update hw04-2-operator.cpp

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -2,25 +2,20 @@
     จงเติมโค้ดโปรแกรมตามที่แสดงในส่วนของ Placeholder เพื่อแสดงผลลัพธ์ของ Operator ด้านซ้ายมือ พร้อมทั้งแก้ไขโค้ดโปรแกรมให้ถูกต้อง
 */
 #include <stdio.h>
-
 int main() {
     int a = 0, b = 0 ;
     printf( "Please enter values (a b) : " ) ;
     scanf( "%d %d", &a, &b ) ;
 
-    // Arithmetic Operators
+    
     printf( "a = %d , b = %d \n", a, b ) ;
     printf( "a + b = %d \n", a + b ) ;
     printf( "a - b = %d \n", a - b ) ;
     printf( "a * b = %d \n", a * b ) ;
-    printf( "a / b = %d (b must be greater than 0)\n", b > 0 ? a / b : 0 ) ;
+    printf( "a / b = %d (b must greater than 0)\n", b > 0 ? a / b : 0 ) ;
     printf( "a %% b = %d \n", a % b ) ;
-    
-    // Increment and Decrement Operators
     printf( "++a = %d \n", ++a ) ;
     printf( "a-- = %d \n", a-- ) ;
-    
-    // Assignment Operators
     printf( "a += 1 %d \n", a += 1 ) ;
     printf( "a += b %d \n", a += b ) ;
     printf( "a -= 1 %d \n", a -= 1 ) ;
@@ -29,10 +24,7 @@ int main() {
     printf( "a *= b %d \n", a *= b ) ;
     printf( "a %%= 1 %d \n", a %= 1 ) ;
     printf( "a %%= b %d \n", a %= b ) ;
-    
-    // Logical Operators
     printf( "a && b %d \n", a && b ) ;
     printf( "a || b %d \n", a || b ) ;
-
     return 0;
 }
