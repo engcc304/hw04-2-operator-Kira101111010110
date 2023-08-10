4bf521665d0b3d83a0ce29ec9a8af9b192e999d0
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-08-10 12:54:00 +0700

    Update hw04-2-operator.cpp

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -2,15 +2,12 @@
     จงเติมโค้ดโปรแกรมตามที่แสดงในส่วนของ Placeholder เพื่อแสดงผลลัพธ์ของ Operator ด้านซ้ายมือ พร้อมทั้งแก้ไขโค้ดโปรแกรมให้ถูกต้อง
 */
 #include <stdio.h>
-
 int main() {
-
     int a = 0, b = 0 ;
     printf( "Please enter value (a b) : " ) ;
     scanf( "%d %d", &a, &b ) ; //TODO: #11 Input variable 'a' and 'b'
 
     //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
-
     printf( "a = %d, b = %d \n", a, b ) ;
     printf( "a + b = %d\n", a + b ) ;
     printf( "a - b = %d\n", a - b ) ;
@@ -29,7 +26,5 @@ int main() {
     printf( "a %%= b %d\n", a %= b ) ;
     printf( "a && b %d\n", a && b ) ;
     printf( "a || b %d\n", a || b ) ;
-
     return 0 ;
-
 }
