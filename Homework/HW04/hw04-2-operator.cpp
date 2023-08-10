0ffadada195c269b19b988d31e73aa336e46f04b
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-08-10 13:18:30 +0700

    Update hw04-2-operator.cpp

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -8,23 +8,23 @@ int main() {
     scanf( "%d %d", &a, &b ) ;
 
     
-    printf( "a = %d , b = %d \n", a, b ) ;
-    printf( "a + b = %d \n", a + b ) ;
-    printf( "a - b = %d \n", a - b ) ;
-    printf( "a * b = %d \n", a * b ) ;
-    printf( "a / b = %d (b must greater than 0)\n", b > 0 ? a / b : 0 ) ;
-    printf( "a %% b = %d \n", a % b ) ;
-    printf( "++a = %d \n", ++a ) ;
-    printf( "a-- = %d \n", a-- ) ;
-    printf( "a += 1 %d \n", a += 1 ) ;
-    printf( "a += b %d \n", a += b ) ;
-    printf( "a -= 1 %d \n", a -= 1 ) ;
-    printf( "a -= b %d \n", a -= b ) ;
-    printf( "a *= 1 %d \n", a *= 1 ) ;
-    printf( "a *= b %d \n", a *= b ) ;
-    printf( "a %%= 1 %d \n", a %= 1 ) ;
-    printf( "a %%= b %d \n", a %= b ) ;
-    printf( "a && b %d \n", a && b ) ;
-    printf( "a || b %d \n", a || b ) ;
+    printf( "a = %d, b = %d \n", a, b ) ;
+    printf( "a + b = %d\n", a + b ) ;
+    printf( "a - b = %d\n", a - b ) ;
+    printf( "a * b = %d\n", a * b ) ;
+    printf( "a / b = %d(b must greater than 0)\n", b > 0 ? a / b : 0 ) ;
+    printf( "a %% b = %d\n", a % b ) ;
+    printf( "++a = %d\n", ++a ) ;
+    printf( "a-- = %d\n", a-- ) ;
+    printf( "a += 1 %d\n", a += 1 ) ;
+    printf( "a += b %d\n", a += b ) ;
+    printf( "a -= 1 %d\n", a -= 1 ) ;
+    printf( "a -= b %d\n", a -= b ) ;
+    printf( "a *= 1 %d\n", a *= 1 ) ;
+    printf( "a *= b %d\n", a *= b ) ;
+    printf( "a %%= 1 %d\n", a %= 1 ) ;
+    printf( "a %%= b %d\n", a %= b ) ;
+    printf( "a && b %d\n", a && b ) ;
+    printf( "a || b %d\n", a || b ) ;
     return 0;
 }
